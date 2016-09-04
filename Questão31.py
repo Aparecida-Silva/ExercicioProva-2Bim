db60d68c688e26f1979ddd56e88d0a91dea81b29
Aparecida Silva <cidinhalilisilva01@gmail.com>
2016-09-03 23:20:22 -0300

    Questão31.py

---
+++ Questão31.py
@@ -0,0 +1,42 @@
+'''
+O Sr. Manoel Joaquim expandiu seus negócios para além dos negócios de 1,99 e agora possui uma
+loja de conveniências. Faça um programa que implemente uma caixa registradora rudimentar. O
+programa deverá receber um número desconhecido de valores referentes aos preços das mercadorias.
+Um valor zero deve ser informado pelo operador para indicar o final da compra. O programa deve
+então mostrar o total da compra e perguntar o valor em dinheiro que o cliente forneceu, para
+então calcular e mostrar o valor do troco. Após esta operação, o programa deverá voltar ao ponto
+inicial, para registrar a próxima compra. A saída deve ser conforme o exemplo abaixo:
+
+Lojas Tabajara 
+Produto 1: R$ 2.20
+Produto 2: R$ 5.80
+Produto 3: R$ 0
+Total: R$ 9.00
+Dinheiro: R$ 20.00
+Troco: R$ 11.00
+...
+'''
+
+print("Lojas Tabajara")
+
+fim = 1
+while (fim != 0):
+	soma = 0
+	a = 1
+	valor = eval(input("Digite o valor do produto ou 0 para finalizar: "))
+	while (valor > 0):
+		soma = soma + valor
+		print("Produto", a, "R$ %.2f" % valor)
+		a = a + 1
+		valor = eval(input("Digite o valor do produto ou 0 para finalizar: "))
+
+	if (soma > 0):
+		print("Total R$: %.2f" % soma)
+		formaPagamento = int(input("Digite (1)-Cartao / (2)-Dinheiro: "))
+		if (formaPagamento == 2):
+			quantDinheiro = eval(input("Informe o valor pago: "))
+			troco = quantDinheiro - soma
+		else:
+			troco = 0
+		print("Troco de R$ %.2f" % troco)
+	fim = int(input("Digite 0 para sair ou 1 para continuar: "))
